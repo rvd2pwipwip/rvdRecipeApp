87c276fe54728c4c45ad0b78516d04a794b10ebc
Herve Desrosiers <hdesrosiers@stingray.com>
2021-02-03 09:31:33 -0500

    Asynchronously Load Images with Glide - video 19

--- app/src/main/java/com/hdesrosiers/rvdrecipeapp/presentation/components/RecipeCard.kt
+++ app/src/main/java/com/hdesrosiers/rvdrecipeapp/presentation/components/RecipeCard.kt
@@ -9,11 +9,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.unit.dp
 import com.hdesrosiers.rvdrecipeapp.R
 import com.hdesrosiers.rvdrecipeapp.domain.model.Recipe
+import com.hdesrosiers.rvdrecipeapp.util.DEFAULT_RECIPE_IMAGE
+import com.hdesrosiers.rvdrecipeapp.util.loadPicture
 
 @Composable
 fun RecipeCard(
@@ -25,7 +28,9 @@ fun RecipeCard(
         modifier = Modifier
             .padding(
                 bottom = 6.dp,
-                top = 6.dp
+                top = 6.dp,
+                start = 8.dp,
+                end = 8.dp
             )
             .fillMaxWidth()
             .clickable(onClick = onClick),
@@ -33,13 +38,16 @@ fun RecipeCard(
     ) {
         Column {
             recipe.featuredImage?.let { url ->
-                Image(
-                    bitmap = imageResource(id = R.drawable.empty_plate), // synchronous load on main thread api!!!
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .preferredHeight(222.dp),
-                    contentScale = ContentScale.Crop
-                )
+                val image = loadPicture(url = url, defaultImage = DEFAULT_RECIPE_IMAGE).value
+                image?.let { img -> // if image not null, go inside lambda
+                    Image(
+                        bitmap = image.asImageBitmap(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .preferredHeight(222.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
             recipe.title?.let { title ->
                 Row(
