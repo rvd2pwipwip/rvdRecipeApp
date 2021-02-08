14688b2892f003bb07e296ca33f00af142b0844f
Herve Desrosiers <hdesrosiers@stingray.com>
2021-02-08 13:39:04 -0500

    Constraint Layout - video 28

--- app/src/main/java/com/hdesrosiers/rvdrecipeapp/presentation/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/hdesrosiers/rvdrecipeapp/presentation/components/CircularIndeterminateProgressBar.kt
@@ -1,13 +1,17 @@
 package com.hdesrosiers.rvdrecipeapp.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.WithConstraints
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.font
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,15 +19,111 @@ fun CircularIndeterminateProgressBar(
     isDisplayed: Boolean
 ) {
     if (isDisplayed) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp),
-            horizontalArrangement = Arrangement.Center
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize(),
         ) {
+            val (progressBar, text) = createRefs()
+            val guideline = createGuidelineFromTop(0.3f)
+
             CircularProgressIndicator(
+                modifier = Modifier
+                    .constrainAs(progressBar) {
+                        centerHorizontallyTo(parent)
+                        top.linkTo(guideline)
+                    },
                 color = MaterialTheme.colors.primary
+            )
+            Text(
+                modifier = Modifier
+                    .constrainAs(text) {
+                        centerHorizontallyTo(progressBar)
+                        top.linkTo(progressBar.bottom)
+                    }
+                    .padding(top = 8.dp),
+                text = "Loading...",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = TextUnit.Companion.Sp(15)
+                )
             )
         }
     }
 }
+
+    /////   decoupled constraints example below
+
+
+//    if (isDisplayed) {
+//        // access screen properties
+//        WithConstraints(
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            val constraints = if (minWidth < 600.dp) { // portrait mode
+//                myDecoupledConstraints(0.3f)
+//            } else {
+//                myDecoupledConstraints(0.7f)
+//            }
+//
+//            ConstraintLayout(
+//                modifier = Modifier.fillMaxSize(),
+//                constraintSet = constraints
+//            ) {
+//                CircularProgressIndicator(
+//                    modifier = Modifier
+//                        .layoutId("progressBar"),
+//                    color = MaterialTheme.colors.primary
+//                )
+//                Text(
+//                    modifier = Modifier
+//                        .layoutId("text")
+//                        .padding(top = 8.dp),
+//                    text = "Loading...",
+//                    style = TextStyle(
+//                        color = Color.Black,
+//                        fontSize = TextUnit.Companion.Sp(15)
+//                    )
+//                )
+//            }
+//        }
+//    }
+//}
+//
+//private fun myDecoupledConstraints(verticalBias: Float): ConstraintSet {
+//    return ConstraintSet {
+//        val guideline = createGuidelineFromTop(verticalBias)
+//        val progressBar = createRefFor("progressBar")
+//        val text = createRefFor("text")
+//
+//        constrain(progressBar) {
+//            top.linkTo(guideline)
+//            start.linkTo(parent.start)
+//            end.linkTo(parent.end)
+//        }
+//
+//        constrain(text) {
+//            top.linkTo(progressBar.bottom)
+//            centerHorizontallyTo(progressBar)
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
